ff9e72af1854d10a3d42ec6be72525be314e6bfa
Abner Rodrigues <abnerluis1001@gmail.com>
2021-02-24 13:53:08 -0300

    feat(users): added account insertion together with user insertion

--- lib/rocketpay_web/views/users.view.ex
+++ lib/rocketpay_web/views/users.view.ex
@@ -1,10 +1,17 @@
 defmodule RocketpayWeb.UsersView do
-  alias Rocketpay.User
+  alias Rocketpay.{Account, User}
   def render(
     "create.json",
-    %{user: %User{id: id, name: name, nickname: nickname, email: email, age: age}
+    %{
+      user: %User{
+        account: %Account{ id: account_id, balance: balance },
+        id: id,
+        name: name,
+        nickname: nickname,
+        email: email,
+        age: age
+      }
   }) do
-
     %{
       message: "User created!",
       user: %{
@@ -12,7 +19,11 @@ defmodule RocketpayWeb.UsersView do
         name: name,
         age: age,
         email: email,
-        nickname: nickname
+        nickname: nickname,
+        account: %{
+          id: account_id,
+          balance: balance
+        }
       }
     }
 
