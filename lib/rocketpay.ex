6679bddfbc9b524b1c9532534a9372cf22648cd0
Abner Rodrigues <abnerluis1001@gmail.com>
2021-02-28 09:44:07 -0300

    feat(accounts): created withdraw action and facade

--- lib/rocketpay.ex
+++ lib/rocketpay.ex
@@ -1,7 +1,8 @@
 defmodule Rocketpay do
   alias Rocketpay.Users.Create, as: CreateUser
-  alias Rocketpay.Accounts.Deposit
+  alias Rocketpay.Accounts.{Deposit, Withdraw}
 
   defdelegate create_user(params), to: CreateUser, as: :call
   defdelegate deposit(params), to: Deposit, as: :call
+  defdelegate withdraw(params), to: Withdraw, as: :call
 end
